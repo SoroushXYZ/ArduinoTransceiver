1c422ad1399a1ed3b03ff5f1fa0c9b1e0e3cba2e
Soroush <soroushsadeghian.ss@gmail.com>
2024-11-28 06:16:27 -0500

    Uses enum instead of menu level that was int

--- Transmitter_Config/MenuManager.h
+++ Transmitter_Config/MenuManager.h
@@ -5,27 +5,33 @@
 #include <LiquidCrystal_I2C.h>
 #include "Channel.h"
 
+// Enum for Menu Levels
+enum MenuLevel {
+    CHANNEL_LIST,
+    CHANNEL_SETTINGS
+};
+
 class MenuManager {
 private:
     LiquidCrystal_I2C* lcd;
     Channel* channels;
     int channelCount;
-    int selectedIndex; // The currently selected channel index
-    int menuLevel;     // 0 = Channel List, 1 = Channel Settings
-    int subMenuIndex;  // The index of the selected setting
-    int scrollOffset;  // The topmost visible channel index
+    int selectedIndex;  // The currently selected channel index
+    MenuLevel menuLevel; // Enum to track the current menu level
+    int subMenuIndex;   // The index of the selected setting
+    int scrollOffset;   // The topmost visible channel index
 
     static const int maxVisibleItems = 4; // Number of lines on the LCD
 
 public:
     MenuManager(LiquidCrystal_I2C* lcd, Channel* channels, int count)
         : lcd(lcd), channels(channels), channelCount(count), selectedIndex(0),
-          menuLevel(0), subMenuIndex(0), scrollOffset(0) {}
+          menuLevel(CHANNEL_LIST), subMenuIndex(0), scrollOffset(0) {}
 
     void displayMenu() {
         lcd->clear();
 
-        if (menuLevel == 0) {
+        if (menuLevel == CHANNEL_LIST) {
             // Display channel list
             for (int i = 0; i < maxVisibleItems; i++) {
                 int channelIndex = scrollOffset + i;
@@ -39,7 +45,7 @@ public:
                 }
                 lcd->print(channels[channelIndex].getName());
             }
-        } else if (menuLevel == 1) {
+        } else if (menuLevel == CHANNEL_SETTINGS) {
             // Display channel settings
             lcd->clear();
             lcd->setCursor(0, 0);
@@ -55,10 +61,7 @@ public:
     }
 
     void updateEncoder(int direction, bool buttonPressed) {
-        Serial.print("Menu Level: ");
-        Serial.println(menuLevel); // Debug current menu level
-
-        if (menuLevel == 0) {
+        if (menuLevel == CHANNEL_LIST) {
             // Navigate channels
             selectedIndex = (selectedIndex + direction + channelCount) % channelCount;
 
@@ -68,33 +71,28 @@ public:
             } else if (selectedIndex >= scrollOffset + maxVisibleItems) {
                 scrollOffset = selectedIndex - maxVisibleItems + 1;
             }
-        } else if (menuLevel == 1) {
+        } else if (menuLevel == CHANNEL_SETTINGS) {
             // Navigate settings
             int itemCount = channels[selectedIndex].getConfigurableItemCount();
             subMenuIndex = (subMenuIndex + direction + itemCount) % itemCount;
         }
 
         if (buttonPressed) {
-            if (menuLevel == 0) {
-                Serial.println("Entering Channel Settings"); // Debug menu transition
-                menuLevel = 1;
-                subMenuIndex = 0; // Start at the first configurable item
-            } else if (menuLevel == 1) {
-                Serial.println("Configuring Item"); // Debug configuration action
+            if (menuLevel == CHANNEL_LIST) {
+                menuLevel = CHANNEL_SETTINGS; // Enter settings
+                subMenuIndex = 0;
+            } else if (menuLevel == CHANNEL_SETTINGS) {
                 lcd->clear();
                 lcd->setCursor(0, 0);
                 lcd->print("HelloWorld!");
                 channels[selectedIndex].configureItem(subMenuIndex);
-                delay(2000); // Pause for feedback
-                menuLevel = 0; // Return to channel list
+                delay(2000);
+                menuLevel = CHANNEL_LIST; // Return to channel list
             }
         }
 
         displayMenu();
     }
-
-    // Getter for current menu level
-    int getMenuLevel() const { return menuLevel; }
 };
 
 #endif
