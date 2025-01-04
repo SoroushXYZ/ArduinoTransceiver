7225e0a9683d5083ebf575dbc85706fbcccb9159
Soroush <soroushsadeghian.ss@gmail.com>
2025-01-03 19:48:08 -0500

    Implemented basic timed updates

---
+++ Transmitter_Config/TimedUpdateHandler.h
@@ -0,0 +1,41 @@
+#ifndef TIMEDUPDATEHANDLER_H
+#define TIMEDUPDATEHANDLER_H
+
+#include <Arduino.h>
+#include "MenuManager.h"
+
+// Global variable to track the last update time
+unsigned long lastUpdateTime = 0;
+
+// Function to handle timed updates
+void handleTimedUpdates(MenuManager& menu) {
+    unsigned long currentTime = millis();
+
+    // Check if 100 ms have passed since the last update
+    if (currentTime - lastUpdateTime >= 100) {
+        switch(menu.getMenuLevel()){
+          case READ_VALUE:
+              updateChannelValues();
+              menu.displayMenu();  // Update the display
+              break;
+          // case CHANNEL_LIST:
+          //     displayChannelList();
+          //     break;
+          // case CHANNEL_SETTINGS:
+          //     displayChannelSettings();
+          //     break;
+          // case SELECT_DEVICE:
+          //     displaySelectDevice();
+          //     break;
+          // case CALIBRATE:
+          //     displayCalibrate();
+          //     break;
+          // case TRIM:
+          //     displayTrim();
+          //     break;
+        }
+        lastUpdateTime = currentTime;  // Reset the last update time
+    }
+}
+
+#endif
